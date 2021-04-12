c10fa3020f752141304a1329a6cb83401da58757
Scott Erwin <scottallenerwin@gmail.com>
2021-04-12 19:45:04 -0400

    created index and updated gitignore and npmed inquirer

---
+++ index.js
@@ -0,0 +1,36 @@
+// TODO: Include packages needed for this application
+const questions = require('inquirer');
+
+// TODO: Create an array of questions for user input
+questions
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your user name?',
+      name: 'username',
+    },
+    {
+      type: 'password',
+      message: 'What is your password?',
+      name: 'password',
+    },
+    {
+      type: 'password',
+      message: 'Re-enter password to confirm:',
+      name: 'confirm',
+    },
+  ])
+  .then((response) =>
+    response.confirm === response.password
+      ? console.log('Success!')
+      : console.log('You forgot your password already?!')
+);
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
