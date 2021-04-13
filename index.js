c683e7fd561acacd8050b7b29962ba1fcf762871
Scott Erwin <scottallenerwin@gmail.com>
2021-04-13 18:34:42 -0400

    updated markdown so table of contents links works and got the write file to work with two option and commented it

--- index.js
+++ index.js
@@ -1,111 +1,127 @@
 // fs library package for reading/writing files
-const fs = require('fs');
+const fs = require("fs");
+
 // TODO: Include packages needed for this application
-const questions = require('inquirer');
+const inquirer = require("inquirer");
 
-// gets information from generateMarkdown.js
-const generateMarkdownInfo = require('./js/generateMarkdown.js');
+// gets Specific functions {gernerateMarkdown} from the export information from generateMarkdown.js
+const { generateMarkdown } = require("./js/generateMarkdown.js");
 
-// TODO: Create an array of questions for user input
-questions
+// 2nd way get all Information from the export of generateMarkdown.js
+// const generateMarkdownInfo = require('./js/generateMarkdown.js');
+
+function promptUser() {
+  // TODO: Create an array of questions for user input
+  inquirer
     .prompt([
-        {
-            type: 'input',
-            name: 'yourProjectTitle',
-            message: 'What is your Project Title?',
-        },
-        {
-            type: 'input',
-            name: 'descriptionMotivation',
-            message: 'What was your motivation?',
-        },
-        {
-            type: 'input',
-            name: 'descriptionBuild',
-            message: 'Why did you build this project?',
-        },
-        {
-            type: 'input',
-            name: 'descriptionProblemSolve',
-            message: 'What problem does it solve?',
-        },
-        {
-            type: 'input',
-            name: 'descriptionLearn',
-            message: 'What did you learn?',
-        },
-        {
-            type: 'confirm',
-            name: 'tableOfContents',
-            message: 'What u want in your Table of Contents',
-        },
-        {
-            type: 'input',
-            name: 'installProject',
-            message: 'What are the steps required to install your project?',
-        },
-        {
-            type: 'input',
-            name: 'usage',
-            message: 'Add screenshots path',
-        },
-        {
-            type: 'input',
-            name: 'usageAltText',
-            message: 'Screenshots Alt text',
-        },
-        {
-            type: 'input',
-            name: 'credits',
-            message: 'List your collaborators',
-        },
-        {
-            type: 'list',
-            name: 'licenses',
-            message: 'Choose an open source license',
-            choices: ['MIT License', 'GNU General Public License v3.0', 'GNU Affero General Public License v3.0'],
-        },
-        {
-            type: 'input',
-            name: 'licensesYear',
-            message: 'Type in the Current Year',
-        },
-        {
-            type: 'input',
-            name: 'licensesFullName',
-            message: 'Type in your Full Name',
-        },
-        {
-            type: 'input',
-            name: 'features',
-            message: 'List your Projects Features',
-        },
-        {
-            type: 'input',
-            name: 'tests',
-            message: 'provide examples on how to test run your project',
-        },
-        {
-            type: 'input',
-            name: 'questions',
-            message: 'Do you have any questions?',
-        },
+      {
+        type: "input",
+        name: "yourProjectTitle",
+        message: "What is your Project Title?",
+      },
+      {
+        type: "input",
+        name: "descriptionMotivation",
+        message: "What was your motivation?",
+      },
+      {
+        type: "input",
+        name: "descriptionBuild",
+        message: "Why did you build this project?",
+      },
+      {
+        type: "input",
+        name: "descriptionProblemSolve",
+        message: "What problem does it solve?",
+      },
+      {
+        type: "input",
+        name: "descriptionLearn",
+        message: "What did you learn?",
+      },
+      {
+        type: "confirm",
+        name: "tableOfContents",
+        message: "What u want in your Table of Contents",
+      },
+      {
+        type: "input",
+        name: "installProject",
+        message: "What are the steps required to install your project?",
+      },
+      {
+        type: "input",
+        name: "usage",
+        message: "Add screenshots path",
+      },
+      {
+        type: "input",
+        name: "usageAltText",
+        message: "Screenshots Alt text",
+      },
+      {
+        type: "input",
+        name: "credits",
+        message: "List your collaborators",
+      },
+      {
+        type: "list",
+        name: "licenses",
+        message: "Choose an open source license",
+        choices: [
+          "MIT License",
+          "GNU General Public License v3.0",
+          "GNU Affero General Public License v3.0",
+        ],
+      },
+      {
+        type: "input",
+        name: "licensesYear",
+        message: "Type in the Current Year",
+      },
+      {
+        type: "input",
+        name: "licensesFullName",
+        message: "Type in your Full Name",
+      },
+      {
+        type: "input",
+        name: "features",
+        message: "List your Projects Features",
+      },
+      {
+        type: "input",
+        name: "tests",
+        message: "provide examples on how to test run your project",
+      },
+      {
+        type: "input",
+        name: "questions",
+        message: "Do you have any questions?",
+      },
     ])
     .then((data) => {
-        // split(' ') identifies space in the input and  join('') repmoves the spaces. So we do not have spaces in the file name.
-        console.log(data.yourProjectTitle);
-        
-        const filename = `${data.yourProjectTitle.split(' ').join('')}.json`;
-        fs.writeFile(filename, generateMarkdownInfo.generateMarkdown(), (err) =>
-        err ? console.log(err) : console.log('Success!')
-        );
-    });
+      // split(' ') identifies space in the input and  join('') repmoves the spaces. So we do not have spaces in the file name.
+      console.log(data);
+      const filename = `${data.yourProjectTitle.split(" ").join("")}.md`;
+      console.log(filename);
 
-// TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
+      // creates file in markdown location with the generateMarkdown input data
+      fs.writeFile(`./markdown/${filename}`, generateMarkdown(data), (err) =>
+        err ? console.log(err) : console.log("Success!")
+      );
+
+      // 2nd way to get data
+      // fs.writeFile(filename, generateMarkdownInfo.generateMarkdown(data), (err) =>
+      // err ? console.log(err) : console.log('Success!')
+      // );
+    });
+}
 
 // TODO: Create a function to initialize app
-// function init() {}
+function init() {
+  promptUser();
+}
 
 // Function call to initialize app
-// init();
+init();
